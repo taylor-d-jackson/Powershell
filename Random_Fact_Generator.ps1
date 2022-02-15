affdb95e8eacfb404933dd1aed42c5dd50a3452d
taylor-d-jackson <59874815+taylor-d-jackson@users.noreply.github.com>
2022-02-15 11:50:03 -0500

    Added random fact generator...
    Uses http://randomfactgenerator.net/

---
+++ Random_Fact_Generator.ps1
@@ -0,0 +1,16 @@
+﻿function factGenerator {
+    param(
+    [string]$intro = "Prepare for a random fact...",
+    [int]$random = (Get-Random -Maximum 1400),
+    [string]$fact= (((Invoke-WebRequest -Uri http://www.randomfactgenerator.net?id=$random).links)."data-text"),
+    [int]$speechRate= 1
+
+    )
+        $factSpeech = "$intro $fact"
+        $voiceObject = New-Object -com SAPI.SpVoice
+        $voice = $voiceObject.getVoices()|where {$_.id -like "*DAVID*"}
+        $voiceObject.voice = $voice
+        $voiceObject.rate = $speechRate
+        $voiceObject.speak($factSpeech)
+}
+factGenerator
